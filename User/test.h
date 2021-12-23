0957e6b6e712ff892ae0d85d9080e56c76ee8bf8
WHALEEYE <70808236+WHALEEYE@users.noreply.github.com>
2021-12-23 15:40:17 +0800

    style: reformatted

--- User/test.h
+++ User/test.h
@@ -2,23 +2,17 @@
 #define TEST_H_
 #include "main.h"
 
-
-
-
-
 #define USART2_MAX_RECV_LEN		50					//最大接收缓存字节数
 #define USART2_MAX_SEND_LEN		50					//最大发送缓存字节数
 
 #define USART1_MAX_RECV_LEN     50
 
 extern uint16_t USART2_RX_STA;
-extern uint8_t  USART2_TX_BUF[USART2_MAX_SEND_LEN];
-extern uint8_t  USART2_RX_BUF[USART2_MAX_RECV_LEN];
+extern uint8_t USART2_TX_BUF[USART2_MAX_SEND_LEN];
+extern uint8_t USART2_RX_BUF[USART2_MAX_RECV_LEN];
 
-
-
-void usart1_printf(char *fmt,...);
-void usart2_printf(char *fmt,...);
+void usart1_printf(char *fmt, ...);
+void usart2_printf(char *fmt, ...);
 void Test(void);
 
 #endif /* TEST_H_ */
--- User/wifi_config.h
+++ User/wifi_config.h
@@ -5,10 +5,9 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-
 uint8_t wifi_quit_trans(void);
 void wifi_echo(uint8_t mode);
-uint8_t wifi_send_cmd(char *cmd,char *ack,uint16_t waittime);
-void wifi_ap_send(uint8_t * data_add,uint8_t len);
+uint8_t wifi_send_cmd(char *cmd, char *ack, uint16_t waittime);
+void wifi_ap_send(uint8_t *data_add, uint8_t len);
 void wifi_init(uint8_t mode);
 #endif /* WIFI_CONFIG_H_ */

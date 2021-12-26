a6b4c7264070ef0badfc3016f93aebeb54425718
KONATA_CG <11911409@mail.sustech.edu.cn>
2021-12-26 13:47:12 +0800

    Update test.c

--- User/test.c
+++ User/test.c
@@ -4,6 +4,8 @@
 #include "usart.h"
 #include "wifi_config.h"
 #include "lcd.h"
+#include "string.h"
+#include "stdlib.h"
 
 uint16_t USART2_RX_STA = 0;
 uint8_t USART2_TX_BUF[USART2_MAX_SEND_LEN];
@@ -17,7 +19,12 @@ uint8_t TIMER_MAX_count = 0;
 uint8_t countdown_flag = 0;
 
 uint8_t STATUS;
-char ANSWER = ' ';
+
+char ANSWER[100];
+char QUESTION[100];
+char MAX_TIME[100];
+char POINT[100];
+char WHOLE_QUESTION[500];
 
 extern TIM_HandleTypeDef htim3;
 
@@ -65,6 +72,7 @@ void Test(void)
 	STATUS = 0;
 	while (1)
 	{
+		// Respondent
 		if (mode)
 		{
 			if (USART1_RX_STA == 1)
@@ -76,45 +84,87 @@ void Test(void)
 			HAL_UART_Receive_DMA(&huart1, USART1_RX_BUF,
 			USART1_MAX_RECV_LEN);
 		}
+		// Questioner
 		else
 		{
 			switch (STATUS)
 			{
+			// Questioning mode
 			case 0:
 				// Wait until the question is received
 				if (USART1_RX_STA == 1 && countdown_flag == 0)
 				{
 					USART1_RX_STA = 0;
-					uint8_t length = strlen((char*) USART1_RX_BUF);
-					if (!strcmp("exit\r\n", USART1_RX_BUF))
+					if (!strcmp("exit\r\n", (char*)USART1_RX_BUF))
 					{
 						STATUS = 2;
 						break;
 					}
-					// Set the answer
-					ANSWER = *(USART1_RX_BUF + length - 1);
+					// Set the 4 part of a whole question
+					char delims[] = "#";
+					char *result = NULL;
+					result = strtok((char*)USART1_RX_BUF, delims);
+					if(result != NULL) {
+						strcpy(QUESTION, result);
+						usart1_printf("%s\r\n", result);
+						usart1_printf("%s\r\n", QUESTION);
+					    result = strtok( NULL, delims );
+					}
+					if(result != NULL) {
+						strcpy(POINT, result);
+						usart1_printf("%s\r\n", result);
+						usart1_printf("%s\r\n", POINT);
+					    result = strtok( NULL, delims );
+					}
+					if(result != NULL) {
+						strcpy(MAX_TIME, result);
+						usart1_printf("%s\r\n", result);
+						usart1_printf("%s\r\n", MAX_TIME);
+					    result = strtok( NULL, delims );
+					}
+					if(result != NULL) {
+						strcpy(ANSWER, result);
+						usart1_printf("%s\r\n", result);
+						usart1_printf("%s\r\n", ANSWER);
+					}
+					// ANSWER = *(USART1_RX_BUF + length - 1);
 					// Send the question to respondent
-					wifi_ap_send(USART1_RX_BUF, length - 2);
+					strncat(WHOLE_QUESTION, "[", 5);
+					strncat(WHOLE_QUESTION, QUESTION, strlen(QUESTION)+5);
+					strncat(WHOLE_QUESTION, "][", 5);
+					strncat(WHOLE_QUESTION, POINT, strlen(POINT)+5);
+					strncat(WHOLE_QUESTION, "][", 5);
+					strncat(WHOLE_QUESTION, MAX_TIME, strlen(MAX_TIME)+5);
+					strncat(WHOLE_QUESTION, "]", 5);
+					usart1_printf("%s\r\n", WHOLE_QUESTION);
+					uint8_t length = strlen(WHOLE_QUESTION);
+					wifi_ap_send((uint8_t*)WHOLE_QUESTION, length);
 					// Clear the buffer and refuse to read question from USART anymore
 					HAL_UART_DMAStop(&huart1);
 					memset((char*) USART1_RX_BUF, 0, USART1_MAX_RECV_LEN);
 					// Start count down
 					usart1_printf("[start count down]");
 					TIMER_count = 0;
-					TIMER_MAX_count = 5; // 还需添加倒计时多少秒
+					TIMER_MAX_count = atoi(MAX_TIME); // 还需添加倒计时多少秒
 					countdown_flag = 1;
 					HAL_TIM_Base_Start_IT(&htim3);
 					STATUS = 1;
 				}
 				break;
+			// Answer-receiving mode
 			case 1:
 				wifi_echo(1);
+				// Still counting down
 				if (countdown_flag == 1) {
 
-				} else {
-					usart1_printf("[count down over in case 1]");
+				}
+				// Finish counting down
+				else
+				{
+					// usart1_printf("[count down over in case 1]");
 				}
 				break;
+			// Judging mode
 			case 2:
 				// EXIT
 				break;
@@ -161,11 +211,12 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 		countdown_flag = 0;
 		TIMER_count = 0;
 		TIMER_MAX_count = 0;
+		// Send message to respondent
+		uint8_t response_message[20] = "[Time Exceed Limit]";
+		wifi_ap_send(response_message, strlen((char*) response_message));
+		// Close the interrupt and open DMA receive
 		HAL_TIM_Base_Stop_IT(&htim3);
 		HAL_UART_Receive_DMA(&huart1, USART1_RX_BUF, USART1_MAX_RECV_LEN);
-		uint8_t response_message[20] = "[Time Exceed Limit]";
-		usart2_printf("[Time Exceed Limit]");
-		wifi_ap_send(response_message, strlen((char*) response_message));
 	}
 }
 
